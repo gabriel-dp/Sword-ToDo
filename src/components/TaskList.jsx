d21f18d33a4940daea4d932417867c2758ae7d09
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2022-01-07 11:29:26 -0300

    fix tasklist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 import Task from './Task.jsx'
 
 const TaskList = ({tasks, handleTaskClick, handleTaskDelete}) => {
-    return (
-        <>
-            {tasks.map((task) => (
-                <Task 
-                    key={task.id}
-                    task={task} 
-                    handleTaskClick={handleTaskClick} 
-                    handleTaskDelete={handleTaskDelete}
-                />
-            ))}
-        </>
-    );
+    if(tasks) {
+        return (
+            <>
+                {
+                    tasks.map((task) => (
+                        <Task 
+                            key={task.id}
+                            task={task} 
+                            handleTaskClick={handleTaskClick} 
+                            handleTaskDelete={handleTaskDelete}
+                        />
+                    ))
+                }
+            </>
+        );
+    }
 };
 
 export default TaskList;
