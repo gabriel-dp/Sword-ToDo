ff205eb0d05e57eeaf727afe514ce56b0caddeb3
GABRIEL MEIRA <gabriel.meira.2004@gmail.com>
2022-01-17 11:49:36 -0300

    fix mobile style

--- src/components/TaskList/Header/styles.js
+++ src/components/TaskList/Header/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const HeaderDiv = styled.div`
     width: 100%;
     height: 30px;
-    color: ${props => props.theme.colors.text};
+    color: ${props => props.theme.title === 'light' ? props.theme.colors.primary : props.theme.colors.text};
     font-size: 30px;
     overflow: hidden; 
     text-overflow: ellipsis;
